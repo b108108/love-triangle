072410f808bea0177c3fa2b05ff5c53849ea4cf4
b108108 <b108@tut.by>
2019-02-25 23:22:09 +0300

    T2-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let length = preferences.length; 
+  let count = 0;  
+  for (let i = 0; i<length; i++) { 
+    if (preferences[i] !== -1) {
+      let first = preferences[i] - 1;
+      if (first == i) continue;
+      let second = preferences[first] - 1;
+      if (first == second) continue;
+      let third = preferences[second] - 1;
+      if (third == second) continue;
+      if (third == i) {
+          count++;
+          preferences[first] = preferences[second] = preferences[third] = -1;
+      }
+    }      
+   }
+   return count;
 };
